0bf9ad017b886b6a71b19a437d449c6cfd259092
rafiq451 <ibab7810@gmail.com>
2025-05-24 09:20:43 +0700

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,59 +1,58 @@
 // Nanti convert ke Bahasa Indonesia
 const config = {
   data: {
-    title: "Pernikahan Fulan & Fulana", // Nanti ini otomatis
-    description:
-      "Kami akan menikah dan mengundang Anda untuk turut merayakan momen istimewa ini.", // Nanti ini dibikin random
-    groomName: "Fulan",
-    brideName: "Fulana",
-    parentGroom: "Bapak Groom & Ibu Groom",
-    parentBride: "Bapak Bride & Ibu Bride",
-    date: "2024-12-24",
-    maps_url: "https://goo.gl/maps/abcdef",
+    title: 'Pernikahan Ali & Raisya', // Nanti ini otomatis
+    description: 'Kami akan menikah dan mengundang Anda untuk turut merayakan momen istimewa ini.', // Nanti ini dibikin random
+    groomName: 'Ali',
+    brideName: 'Raisya',
+    parentGroom: 'Bapak Groom & Ibu Groom',
+    parentBride: 'Bapak Bride & Ibu Bride',
+    date: '2025-05-29',
+    maps_url: 'https://maps.app.goo.gl/SScwqnP77jYoTD7m7',
     maps_embed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0000000000005!2d106.8270733147699!3d-6.175392995514422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4f1b6d7b1e7%3A0x2e69f4f1b6d7b1e7!2sMonumen%20Nasional!5e0!3m2!1sid!2sid!4v1633666820004!5m2!1sid!2sid",
-    time: "16:16 - 17:30 WIB",
-    location: "Grand Ballroom, Hotel Majesty",
-    address: "Jl. Jend. Sudirman No.1, Jakarta",
-    ogImage: "/images/og-image.jpg",
-    favicon: "/images/favicon.ico",
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1212.218536953949!2d106.62717782339195!3d-6.65254380594772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69d7392f142f71%3A0xc81fd9995af84a87!2sToko%20Bapak%20Ajo!5e0!3m2!1sen!2sid!4v1748047006047!5m2!1sen!2sid',
+    time: '08:00 WIB - Selesai',
+    location: 'Sinar Jaya Area, RT.02/RW.01',
+    address: 'Purasari, Leuwiliang District, Bogor Regency, West Java 16640',
+    ogImage: '/images/og-image.jpg',
+    favicon: '/images/favicon.ico',
     agenda: [
       {
-        title: "Akad Nikah",
-        date: "2024-12-24",
-        startTime: "16:16",
-        endTime: "17:30",
-        location: "Grand Ballroom, Hotel Majesty",
-        address: "Jl. Jend. Sudirman No.1, Jakarta",
+        title: 'Akad Nikah',
+        date: '2024-05-9',
+        startTime: '08:00',
+        endTime: 'Selesai',
+        location: 'Sinar Jaya Area, RT.02/RW.01',
+        address: 'Purasari, Leuwiliang District, Bogor Regency, West Java 16640',
       },
       {
-        title: "Resepsi Nikah",
-        date: "2024-12-24",
-        startTime: "16:16",
-        endTime: "17:30",
-        location: "Grand Ballroom, Hotel Majesty",
-        address: "Jl. Jend. Sudirman No.1, Jakarta",
-      }
+        title: 'Resepsi Nikah',
+        date: '2025-05-29',
+        startTime: '08:00',
+        endTime: 'Selesai',
+        location: 'Sinar Jaya Area, RT.02/RW.01',
+        address: 'Purasari, Leuwiliang District, Bogor Regency, West Java 16640',
+      },
     ],
     audio: {
-      src: "/audio/fulfilling-humming.mp3", // or /audio/nature-sound.mp3
-      title: "Fulfilling Humming", // or Nature Sound
+      src: '/audio/fulfilling-humming.mp3', // or /audio/nature-sound.mp3
+      title: 'Fulfilling Humming', // or Nature Sound
       autoplay: true,
-      loop: true
+      loop: true,
     },
     banks: [
       {
-        bank: "Bank Central Asia",
-        accountNumber: "1234567890",
-        accountName: "FULAN",
+        bank: 'Bank Central Asia',
+        accountNumber: '3480870394',
+        accountName: 'Alimudin',
       },
       {
-        bank: "Bank Mandiri",
-        accountNumber: "0987654321",
-        accountName: "FULANA",
-      }
-    ]
-  }
+        bank: 'Dana',
+        accountNumber: '083866777745',
+        accountName: 'ALIMUDIN',
+      },
+    ],
+  },
 };
 
 export default config;
